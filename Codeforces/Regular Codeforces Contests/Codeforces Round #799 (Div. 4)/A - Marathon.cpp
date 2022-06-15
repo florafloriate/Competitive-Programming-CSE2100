aab48bd54bedd3af97750fd9eb94fde9e6c31019
Farhana Rahman <71012533+florafloriate@users.noreply.github.com>
2022-06-15 10:45:28 +0600

    Create A - Marathon.cpp

---
+++ Codeforces/Regular Codeforces Contests/Codeforces Round #799 (Div. 4)/A - Marathon.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define INF              1e9
+#define pi               acos(-1)
+#define ll               long long
+#define endl             "\n"
+#define F                first
+#define S                second
+#define LL_INF           1LL<<62
+#define twopow(x)        (1LL<<x)
+#define Checkbit(x, k)   (x & (1LL << k)) ///whether the k-th bit of x is zero or not
+#define mem(name,value)  memset(name, value, sizeof(name))
+#define binary(x)        bitset<31>(x)
+#define decimal(x)       fixed<<setprecision(x)
+#define fastio           ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fastio;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        int a,b,c,d; cin>>a>>b>>c>>d;
+
+        int ans=0;
+        if(b>a) ans++;
+        if(c>a) ans++;
+        if(d>a) ans++;
+
+        cout<<ans<<endl;
+
+
+    }
+}
