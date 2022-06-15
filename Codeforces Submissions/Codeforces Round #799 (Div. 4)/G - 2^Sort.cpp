238f6a78576562d567d5f5672491b01f3be4d2c0
Farhana Rahman <71012533+florafloriate@users.noreply.github.com>
2022-06-15 09:24:37 +0600

    Create G - 2^Sort.cpp

---
+++ Codeforces Submissions/Codeforces Round #799 (Div. 4)/G - 2^Sort.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#define mod              1000000007
+#define INF              1e9
+#define pi               acos(-1)
+#define ll               long long
+#define endl             "\n"
+#define F                first
+#define S                second
+#define LL_INF           1LL<<62
+#define twopow(x)        (1LL<<x)
+#define Checkbit(x, k)   (x & (1LL << k)) ///whether the k-th bit of x is zero or not
+#define mem(name,value)  memset(name, value, sizeof(name))
+#define binary(x)        bitset<31>(x)
+#define decimal(x)       fixed<<setprecision(x)
+#define fastio           ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fastio;
+
+    int t; cin>>t;
+    while(t--)
+    {
+        ll n,k; cin>>n>>k;
+        ll a[n+5];
+        for(int i=0; i<n; i++)
+            cin>>a[i];
+
+        int ok=1;
+        ll ans=0;
+        for(int i=1; i<n; i++)
+        {
+            if(a[i-1]<a[i]*2LL)
+                ok++;
+            else
+                ok=1;
+
+            if(ok>k)
+                ans++;
+        }
+
+        cout<<ans<<endl;
+    }
+}
